99e591a7a85f8b44b1dc4c142097cbadc07b3b49
Tomás R <105678899+70m9@users.noreply.github.com>
2023-10-17 17:18:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let colSelected = 0;
     let playerBpieces = 12;
     let playerWpieces = 12;
+
+
+
+
+    function tupleToString(tuple) {
+        return JSON.stringify(tuple);
+    }
+    
+    // Function to convert a string back to a tuple
+    function stringToTuple(str) {
+        return JSON.parse(str);
+    }
+
+
     // Função para gerar o tabuleiro com base no tamanho selecionado
     function generateBoard() {
         if (boardSize === 6){
@@ -56,54 +70,180 @@ document.addEventListener('DOMContentLoaded', () => {
     // Função para lidar com o clique em uma célula
 
     function handleCellClick(row, col) {
-        let possible = possible_play(row, col, currentPlayer);
+        
 
-        if (!isGameActive ) return;
+        if (!isGameActive) return;
 
         if (putPhase) {
             // Verifique se o jogador ainda tem peças disponíveis
             //########## POR IF POSSIBLE FORA #################//
-            if (currentPlayer === '1' && playerBpieces > 0) {
-                if (possible) {
-                    renderBoard();
-                    playerBpieces--;
-                } else {
-                    alert('Jogada Inválida');
+            let possible_place = possible_play(row, col, currentPlayer);
+
+            if (possible_place){
+
+                if (currentPlayer === '1' && playerBpieces > 0) {
+                        renderBoard();
+                        playerBpieces--;
+                
+                } else if (currentPlayer === '2' && playerWpieces > 0) {
+                        renderBoard();
+                        playerWpieces--;
                 }
-            } else if (currentPlayer === '2' && playerWpieces > 0) {
-                if (possible) {
-                    renderBoard();
-                    playerWpieces--;
-                } else {
-                    alert('Jogada Inválida');
-                }
+
+
+                // Troca o jogador atual
+                currentPlayer = currentPlayer === '1' ? '2' : '1';
+                currentPlayerDisplay.textContent = currentPlayer;
+
+
             } else {
-                alert('Você já colocou todas as peças permitidas durante a fase de colocação.');
+                console.log('Joga Inválida');
             }
 
             console.log(playerBpieces, playerWpieces);
 
             if (playerBpieces === 0 && playerWpieces === 0) {
                 putPhase = false;
+                console.log('Você já colocou todas as peças permitidas durante a fase de colocação.');
                 currentPlayer = '1'; // ou 'Preto', dependendo de quem deve começar após a fase de colocação
                 currentPlayerDisplay.textContent = currentPlayer;
             }
-        } else {
-            alert('Jogada Inválida');
         }
 
-        console.log('aqui');
-        // Troca o jogador atual
-        if (possible){
-            if (currentPlayer === '1') {
-                currentPlayer = '2';
-            } else {
-                currentPlayer = '1';
-            }
-            currentPlayerDisplay.textContent = currentPlayer;
-        }
+        else{
 
+            if (possible_click(row, col, currentPlayer)) {
+                possiblePlays(currentPlayer);
+                
+                console.log(possiblePlays_1);
+                console.log("escolhi peça");
+                
+                //if (can_move(row,col,currentPlayer)){
+
+       
+            //}
+
+
+
+        }
+        
+
+        
     }
+}
+
+
+    // Declare um mapa para armazenar as jogadas possíveis
+const possiblePlays_1 = new Map();
+const possiblePlays_2 = new Map();
+
+function checkDirections(i, j, x, y, currentPlayer) {
+
+    console.log(x + " " + y + " " + board[x][y] + "celula atual: " + board[i][j] + " " + i + " " + j);
+    if (x < 0 || x >= boardSize || y < 0 || y >= boardSize || board[x][y] !== '') {
+        
+        return;
+    } else {
+
+        if (currentPlayer === '1') {
+            const key = tupleToString([i, j]);
+            if (!possiblePlays_1.has(key)) {
+                possiblePlays_1.set(key, []);
+            }
+            possiblePlays_1.get(key).push([x, y]);
+        }
+        else {
+            const key = tupleToString([i, j]);
+            if (!possiblePlays_2.has(key)) {
+                possiblePlays_2.set(key, []);
+            }
+            possiblePlays_2.get(key).push([x, y]);
+        }
+    }
+}
+
+function possiblePlays(currentPlayer) {
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            if (board[i][j] === currentPlayer) {
+                checkDirections(i, j, i + 1, j, currentPlayer);
+                checkDirections(i, j, i, j + 1, currentPlayer);
+                checkDirections(i, j, i - 1, j, currentPlayer);
+                checkDirections(i, j, i, j - 1, currentPlayer);
+            }
+        }
+    }
+}
+
+
+
+
+    function possible_click(i, j, currentPlayer) {
+        if (board[i][j] === currentPlayer) {
+            console.log('true - Cell is yours');
+            return true;
+        }
+        console.log('false - Cell is not yours');
+        return false;
+    }
+    
+    function possible_play(i, j, currentPlayer) {
+        // Check is Empty
+        if (board[i][j] !== '') {
+            console.log('false - Cell is not empty');
+            return false;
+        }
+    
+        if (!putPhase) {
+            board[rowSelected][colSelected] = 0;
+        }
+    
+        board[i][j] = currentPlayer;
+    
+        // Check Horizontal
+        let leftlim = Math.max(0, j - 3);
+        let rightlim = Math.min(boardSize - 1, j);
+
+        console.log("leftlim " + leftlim + " rightlim " + rightlim);
+
+        for (let k = leftlim; k <= rightlim; k++) {
+            console.log("|" + currentPlayer + "|" + board[i][k] + "|" + board[i][k + 1] + "|" +  board[i][k + 2] + "|" + board[i][k + 3]) + "|";
+            if (
+            currentPlayer === board[i][k] &&
+            currentPlayer === board[i][k + 1] &&
+            currentPlayer === board[i][k + 2] &&
+            currentPlayer === board[i][k + 3]
+        ) {
+            board[i][j] = '';
+            return false;
+        }
+        }
+    
+        // Check Vertical
+        leftlim = Math.max(0, i - 3);
+        rightlim = Math.min(boardSize - 1, i);
+    
+        for (let k = leftlim; k <= rightlim; k++) {
+            if (
+            currentPlayer === board[k][j] &&
+            currentPlayer === board[k + 1][j] &&
+            currentPlayer === board[k + 2][j] &&
+            currentPlayer === board[k + 3][j]
+        ) {
+            board[i][j] = '';
+            return false;
+        }
+        }
+        
+        if (!putPhase) {
+            board[rowSelected][colSelected] = currentPlayer;
+        }
+        console.log('true - Move is possible');
+        return true;
+    }
+    
+
+
 
 
 
@@ -117,9 +257,9 @@ document.addEventListener('DOMContentLoaded', () => {
     
                     // Verifica a classe da célula e define o background-image correspondente
                     if (board[row][col] === '1') {
-                        cell.style.backgroundImage = 'url(/assets/"white.png")'; // Substitua o caminho correto da imagem
+                        cell.style.backgroundImage = 'url(/assets/"white.png")'; // Substitua o caleftlimho correto da imagem
                     } else if (board[row][col] === '2') {
-                        cell.style.backgroundImage = 'url("/assets/black.png")'; // Substitua o caminho correto da imagem
+                        cell.style.backgroundImage = 'url("/assets/black.png")'; // Substitua o caleftlimho correto da imagem
                     } else {
                         cell.style.backgroundImage = 'none'; // Limpa o background-image se não houver jogador na célula
                     }
@@ -136,9 +276,9 @@ document.addEventListener('DOMContentLoaded', () => {
     
                     // Verifica a classe da célula e define o background-image correspondente
                     if (board[row][col] === '1') {
-                        cell.style.backgroundImage = 'url("/assets/white.png")'; // Substitua o caminho correto da imagem
+                        cell.style.backgroundImage = 'url("/assets/white.png")'; // Substitua o caleftlimho correto da imagem
                     } else if (board[row][col] === '2') {
-                        cell.style.backgroundImage = 'url("/assets/black.png")'; // Substitua o caminho correto da imagem
+                        cell.style.backgroundImage = 'url("/assets/black.png")'; // Substitua o caleftlimho correto da imagem
                     } else {
                         cell.style.backgroundImage = 'none'; // Limpa o background-image se não houver jogador na célula
                     }
@@ -147,67 +287,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function possible_play(i, j, currentPlayer) {
-        // Check is Empty
-        if (board[i][j] !== '') {
-            console.log('false - Cell is not empty');
-            return false;
-        }
-    
-        if (!putPhase) {
-            board[rowSelected][colSelected] = 0;
-        }
-    
-        board[i][j] = currentPlayer;
-    
-        // Check Horizontal
-        let min = Math.max(0, j - 3);
-        let max = Math.min(boardSize - 1, j);
 
-        console.log("min " + min + " max " + max);
-
-        for (let k = min; k <= max; k++) {
-            console.log("|" + currentPlayer + "|" + board[i][k] + "|" + board[i][k + 1] + "|" +  board[i][k + 2] + "|" + board[i][k + 3]) + "|";
-            if (
-            currentPlayer === board[i][k] &&
-            currentPlayer === board[i][k + 1] &&
-            currentPlayer === board[i][k + 2] &&
-            currentPlayer === board[i][k + 3]
-        ) {
-            board[i][j] = '';
-            if (!putPhase) {
-                board[rowSelected][colSelected] = currentPlayer;
-            }
-            return false;
-        }
-        }
-    
-        // Check Vertical
-        min = Math.max(0, i - 3);
-        max = Math.min(boardSize - 1, i);
-    
-        for (let k = min; k <= max; k++) {
-            if (
-            currentPlayer === board[k][j] &&
-            currentPlayer === board[k + 1][j] &&
-            currentPlayer === board[k + 2][j] &&
-            currentPlayer === board[k + 3][j]
-        ) {
-            board[i][j] = '';
-            if (!putPhase) {
-                board[rowSelected][colSelected] = currentPlayer;
-            }
-            return false;
-        }
-        }
-        
-        if (!putPhase) {
-            board[rowSelected][colSelected] = currentPlayer;
-        }
-        console.log('true - Move is possible');
-        return true;
-    }
-    
 
     // Event listener para o botão "Iniciar Jogo"
     startGameButton.addEventListener('click', () => {
